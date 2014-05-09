31f6f86de853ec464058b7b9809aa3f2c3c9e1cb
avanty <avanty@avanty-X450VC.(none)>
2014-05-09 10:16:52 +0800

    word_break

--- Word_Break.cpp
+++ Word_Break.cpp
@@ -10,13 +10,56 @@
 //Return true because "leetcode" can be segmented as "leet code".	
 
 #include<iostream>
-#include<set>
+#include<unordered_set>
 #include<string>
-
+#include<vector>
 using namespace std;
+/*
+//TLE RECERTIVE
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
-        
+	int i = 0;
+	int j = 1;
+	bool onereal = false;
+	if(s.empty()) return true;
+	for(i = 0 ; i < s.size(); i++){
+	    if(dict.find(s.substr(0,i+1)) != dict.end() ){
+		onereal = onereal || wordBreak(s.substr(i+1,s.size()-i-1), dict);
+	    }
+	}
+	return onereal;
     }
 };
+*/
+
+//DP 
+//0<i<j
+//isword(s.substr(0,j)) = isword(s.substr(0,i)) && isword(s.substr(i,j));
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+	int i,j;
+	vector<bool> isword(s.length()+1,false);
+	isword[0] = true;
+	for(i = 1; i <= s.length(); i++){
+	    for(j = 0; j< i; j++){
+		if(isword[j] && dict.find(s.substr(j,i-j)) != dict.end() ){
+		    isword[i] = true;
+		    break;
+		}
+	    }
+	}
+	return isword[s.length()];
+    }
+};
+
+int main(){
+    string s = "leetcode";
+    unordered_set<string> dict;
+    dict.insert("leet");
+    dict.insert("code");
+    Solution sl;
+    cout<<sl.wordBreak(s, dict)<<endl;
+    return 0;
+}
